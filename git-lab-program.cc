4d0b2bdfc0f259691e570ec08aef7b1e1247a1ec
Joshua-Lollis <joshualollis21@gmail.com>
2025-01-27 14:47:22 -0500

    initial

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joshua Lollis
+ *        Date: January 27, 2025
+ * Description: Transferring things from vsc to github
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
